ed6742dc430128274ed847517244b31016486e0f
KYogesh20 <yogesh201201@gmail.com>
2022-12-19 00:27:54 +0530

    changes: fixed minor errors

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ const { sendMsg } = require("./controllers/MessageController.js");
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const frontendURL = import.meta.env.FRONTEND_URL;
+const frontendURL = process.env.FRONTEND_URL;
 
 const io = new Server(server, {
   cors: {
