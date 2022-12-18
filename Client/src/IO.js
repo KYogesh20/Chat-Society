6a735620dc5e15e5558e0b5f3b7f60b3ee12ee12
KYogesh20 <yogesh201201@gmail.com>
2022-12-19 00:49:37 +0530

    changes: added import.meta instead of process in IO.js

--- Client/src/IO.js
+++ Client/src/IO.js
@@ -1,3 +1,2 @@
 import { io } from "socket.io-client";
-import { env } from "node:process";
-export const socket = io(env.VITE_APP_BACKEND_URL);
+export const socket = io(import.meta.env.VITE_APP_BACKEND_URL);
